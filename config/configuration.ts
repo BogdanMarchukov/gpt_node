860d0cb813032d559eeeaaaee6b78a25d5df4e42
bogdan.marchukov <bogdan.marchukov@wilix.ru>
2023-04-10 18:34:48 +0300

    create config and connect to postgres

---
+++ config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  database: {
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  },
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
-import { GptModule } from './gpt/gpt.module';
+import { GptModule } from './modules/gpt/gpt.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from '../config/configuration';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [GptModule],
+  imports: [
+    GptModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get('database.dialect') || 'postgres',
+        host: configService.get('database.host') || '127.0.0.1',
+        port: +configService.get('database.port') || 5433,
+        username: configService.get('database.userName') || 'postgres',
+        password: configService.get('database.password') || 'mysecretpassword',
+        database: configService.get('database.database') || 'postgres',
+        models: [],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
