8ee7af77f658f2160a8f95b5a64ea1891e22c586
bogdan.marchukov <bogdan.marchukov@wilix.ru>
2023-05-10 23:38:19 +0300

    create response validate

--- src/models/UserChat.ts
+++ src/models/UserChat.ts
@@ -8,19 +8,25 @@ import {
 import { BaseModel } from './Base.model';
 import { Message, Usage } from '../common/types';
 import { User } from './User.model';
-
+import { Exclude, Expose } from 'class-transformer';
+import { IsString } from 'class-validator';
+@Exclude()
 @Table({ tableName: 'userChats', modelName: 'userChat' })
 export class UserChat extends BaseModel<UserChat> {
   @Column
+  @Expose()
   object: string;
 
   @Column
+  @Expose()
   model: string;
 
   @Column(DataType.JSONB)
+  @Expose()
   usage: Usage;
 
   @Column(DataType.JSONB)
+  @Expose()
   message: Message[];
 
   @BelongsTo(() => User, 'userId')
