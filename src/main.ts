c21db01b35a13f966c6c498a26408ff9c5d25053
bogdan.marchukov <bogdan.marchukov@wilix.ru>
2023-05-22 22:40:26 +0300

    add Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -8,7 +9,7 @@ async function bootstrap() {
     {
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://localhost:5672'],
+        urls: [`amqp://${process.env.RMQ_HOST}:5672`],
         queue: 'gpt_queue',
         queueOptions: {
           durable: false,
